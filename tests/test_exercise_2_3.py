208fc87384e0379642ba3212c4c3e562ad19a2a8
David Ham <David.Ham@imperial.ac.uk>
2021-01-20 21:36:56 +0000

    Current test versions.

--- tests/test_exercise_2_3.py
+++ tests/test_exercise_2_3.py
@@ -1,6 +1,17 @@
-from math_utils.primes import isprime
+import pytest
 
-def test_isprime():
-    v = [(1, False), (2, True), (3, False), (4, True)]
-    for t in v:
-        assert isprime(t[0]) == t[1]
+
+def test_import_isprime():
+    from math_utils.primes import isprime
+
+
+@pytest.mark.parametrize("value, prime", [(1, False), 
+                                          (2, True), 
+                                          (3, True), 
+                                          (4, False),
+                                          (47, True),
+                                          (100, False)])
+def test_isprime(value, prime):
+    from math_utils.primes import isprime
+
+    assert isprime(value) == prime, "Incorrect return value from isprime"
--- tests/test_exercise_2_5.py
+++ tests/test_exercise_2_5.py
@@ -1,6 +1,11 @@
-from math_utils import isprime
 
-def test_isprime():
-    v = [(1, False), (2, True), (3, False), (4, True)]
-    for t in v:
-        assert isprime(t[0]) == t[1]
+def test_import():
+    '''Ensure we can import isprime in the new location.'''
+    from math_utils import isprime
+
+
+def test_import_correct_function():
+    from math_utils import isprime, primes
+
+    assert primes.isprime is isprime, \
+        "math_utils.isprime is not the same function as math_utils.primes.isprime"
